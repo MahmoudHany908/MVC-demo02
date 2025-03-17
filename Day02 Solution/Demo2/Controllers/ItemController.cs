0d9ff217bb3b8786be9b604826b5909823c1e758
Mahmoud Hany <2021170936@cis.asu.edu.eg>
2025-03-17 23:45:26 +0200

    Pushing Demo

---
+++ Day02 Solution/Demo2/Controllers/ItemController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+
+namespace Demo2.Controllers
+{
+    public class ItemController : Controller
+    {
+        [NonAction]
+        public void DeleteItem()
+        {
+
+        }
+
+
+        public string Index()
+        {
+            return "Hello from index";
+        }
+
+
+
+        //baseURL/item/GetItem/10
+
+        //I could also remove id from the pattern there and then the URL will be like this to access item with specific id and name
+
+        //baseURL/item/GetItem/?Id=10&?itemName=Chair
+
+        #region Example01
+        //[HttpGet]
+        //public ContentResult GetItem(int? id, string itemName)
+        //{
+
+        //    //ContentResult res = new ContentResult();
+        //    //res.Content = $"Item with name={itemName} </br> and id = {id}";
+        //    //res.ContentType = "text/html";
+        //    //res.StatusCode = 700;//We don't use it usuallly, cuz why :)
+        //    //return res;
+
+
+        //    //In case function uses string not ContentResult
+        //    //return $"Item with name={itemName} and id = {id}";
+
+
+        //    //Easiest approach is using nhelper method instead of creating an object
+
+        //    return Content($"Item with name={itemName} </br> and id = {id}", "text/html");
+
+
+        //}
+
+        #endregion
+
+
+        [HttpGet]
+        public IActionResult GetItem(int? id, string itemName)
+        {
+            // Id = 0 -> Bad Request
+            // Id < 10 -> Not Found
+            // Id >= 10 -> Return Movie
+
+            if (id == 0)
+                return BadRequest();
+            else if (id < 10)
+                return NotFound();
+            else
+                return Content($"Movie With Name = {itemName} And Id = {id}", contentType: "text/html");
+
+
+        }
+
+
+        public IActionResult TestRedirectAction()
+        {
+            //return Redirect("localhost//");
+
+            //when using a URL that changes frequently it's better to use this
+
+            //return RedirectToAction("GetItem", "Item", new {id=10,itemName="Test"});
+
+            //Safer approach to not use strings
+            //return RedirectToAction(nameof(GetItem), "Item", new { id = 10, itemName = "Test" });
+
+            //Note: I can't use nameof with the COntroller as it'll return the fullname "ItemController"
+            //which will throw an exception and I'll have to use splitting
+            //So for controller, we could use String normally but make sure it's 100% correct
+
+            //I could also use redirect to route
+            return RedirectToRoute("Default", new { Controller = "Item", Action = "GetItem", id = 10 });
+
+        }
+
+
+
+    }
+}
---
+++ Day02 Solution/Demo2/Program.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Routing.Constraints;
+
+namespace Demo2
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+            var app = builder.Build();
+
+            //app.MapGet("/", () => "Hello World!");
+            //app.MapGet("/Index", () => "Hello Index!");
+            app.MapControllerRoute(
+                name: "Default",
+                pattern:"{controller=Item}/{action=Index}/{Id:regex(^\\d{{2}}$)}"//I wrote here id with capital I, yet VS can still recongize it and bind it with it's action
+                                                      //id is optional
+                                                      //So I can access Index too without making a new route
+                                                      //Also if I don't send id it will add the default value(Zero)
+                                                      //A good practice is to make this variable nullable also for the action as you mentioned here it's not mandatory so it's logical :)
+                                                      //And instead now, it'll set the id to null if I don't send an id(So I can Identify that not item zero, but no id was sent at all in the 1st place)
+                                                      //instead of using Defaults or contraints I added them directly also
+                
+                //defaults: new { action = "Index", controller = "Item" },
+               
+                //constraints:new {id=new IntRouteConstraint()} //if value sent not int handle it(Views page not found cuz I am not supposed to reach something if I send "Mah" as id)
+                //            new {id=@"\d{2}"}
+                );                                   
+                                                     
+            //Variable Segment(Write your name in the URL and it'll display it)
+
+            //app.MapGet("/{name}", async context =>
+            //{
+            //    var Name = context.GetRouteValue("name");
+            //    await context.Response.WriteAsync($"Hello {Name}");
+
+            //}
+            //);
+
+            //Mixed Segment
+
+            //Must write X then then put whatever you want in the name
+            app.MapGet("/X{name}", async context =>
+            {
+                //Made it directly without creating a variable
+                await context.Response.WriteAsync($"Hello {context.Request.RouteValues["name"]}");
+
+            }
+            );
+
+            app.Run();
+        }
+    }
+}
